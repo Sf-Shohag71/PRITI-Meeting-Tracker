91df9f09722d1244abb2fcdad188cb40a25a67fb
Sf-Shohag71 <shakhfarid71@gmail.com>
2025-07-20 13:02:44 +0600

    Designed navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,20 @@
+import { Calendar, Plus } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function Navigation() {
-  return "Navigation";
+  return (
+    <nav className="bg-secondary p-4 sm:px-6 py-4 flex gap-4 flex-wrap justify-between items-center text-primary 2xl:bg-tertiary 2xl:text-white *:px- *:py-2 *:rounded *:flex *gap-2 *:border">
+      <Link to="/" className="border-primary 2xl:border-white px-4 py-2 gap-2">
+        <Calendar />
+        Events
+      </Link>
+      <Link
+        to="/create"
+        className="border-primary 2xl:border-white px-4 py-2 gap-2"
+      >
+        <Plus />
+        Create an Event
+      </Link>
+    </nav>
+  );
 }
